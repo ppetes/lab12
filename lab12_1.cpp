4d1d8c29afad28381a55e49cb1d3fb4fec865c3d
ratchapon prangthong <ppete2547@gmail.com>
2023-01-06 16:49:47 +0700

    lab_12 1 fish

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,55 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+
+int main(){
+
+
+
+cout << "Press Enter 3 times to reveal your future." ;
+cin.get();
+cin.get();
+cin.get();
+srand(time(0));
+int x = rand()%9;
+switch(x){
+    case 0:
+    cout << "You will get A in this 261102." ;
+    break;
+    case 1:
+    cout << "You will get B+ in this 261102." ;
+    break;
+    case 2:
+    cout << "You will get B in this 261102." ;
+    break;
+    case 3:
+    cout << "You will get C+ in this 261102." ;
+    break;
+    case 4:
+    cout << "You will get C in this 261102." ;
+    break;
+    case 5:
+    cout << "You will get D in this 261102." ;
+    break;
+    case 6:
+    cout << "You will get D+ in this 261102." ;
+    break;
+    case 7:
+    cout << "You will get F in this 261102." ;
+    break;
+    case 8:
+    cout << "You will get W in this 261102." ;
+    break;
+    
+}
+
+return 0;
+
+
+
+
+
+}
